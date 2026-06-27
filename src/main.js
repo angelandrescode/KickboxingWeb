be24ea9d36bdc0e8657b68d84a2db3870b1c8f9e
Angel Andres <andressamaniegosandoval@gmail.com>
2026-06-27 20:41:11 +0200

    Web finished

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Swiper from "swiper";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+
+const swiper = new Swiper(".swiper", {
+  modules: [Autoplay],
+  slidesPerView: 4,
+  spaceBetween: 40,
+  autoplay: {
+    delay: 2000,
+    pauseOnMouseEnter: true,
+  },
+  loop: true,
+});
